cb616f2b280979f61ace5b36f03806a5bfd6de99
rex <rex>
2022-12-12 23:09:05 +0800

    測試

--- dist/resthubs_v2-9441e1aaffd6b818.js
+++ dist/resthubs_v2-9441e1aaffd6b818.js
@@ -600,12 +600,12 @@ function getImports() {
     imports.wbg.__wbindgen_throw = function(arg0, arg1) {
         throw new Error(getStringFromWasm0(arg0, arg1));
     };
-    imports.wbg.__wbindgen_closure_wrapper3506 = function(arg0, arg1, arg2) {
-        const ret = makeMutClosure(arg0, arg1, 244, __wbg_adapter_16);
+    imports.wbg.__wbindgen_closure_wrapper3423 = function(arg0, arg1, arg2) {
+        const ret = makeMutClosure(arg0, arg1, 259, __wbg_adapter_16);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper4109 = function(arg0, arg1, arg2) {
-        const ret = makeMutClosure(arg0, arg1, 265, __wbg_adapter_19);
+    imports.wbg.__wbindgen_closure_wrapper4010 = function(arg0, arg1, arg2) {
+        const ret = makeMutClosure(arg0, arg1, 280, __wbg_adapter_19);
         return addHeapObject(ret);
     };
 
@@ -643,7 +643,7 @@ function initSync(module) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('resthubs_v2-a42351cc5b5a1c62_bg.wasm', import.meta.url);
+        input = new URL('resthubs_v2-9441e1aaffd6b818_bg.wasm', import.meta.url);
     }
     const imports = getImports();
 
--- view/src/nav.rs
+++ view/src/nav.rs
@@ -1,7 +1,7 @@
 use yew::prelude::*;
 
-#[function_component(NavigationBar)]
-fn Nav() -> Html {
+#[function_component]
+pub fn Nav() -> Html {
     html! {
                     <>
                         <head>

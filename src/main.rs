bab05460e352f80face62f6bfeb7bc735cf198a0
rex <rex>
2022-12-12 22:54:50 +0800

    init:

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+use patternfly_yew::*;
+use view::nav;
+use yew::prelude::*;
+#[function_component]
+fn App() -> Html {
+   
+    let counter = use_state(|| 0);
+    let onclick = {
+        let counter = counter.clone();
+        move |_| {
+            let value = *counter + 1;
+            counter.set(value);
+        }
+    };
+
+    html! {
+        <div>
+            <button {onclick}>{ "+1" }</button>
+            <p>{ *counter }</p>
+        </div>
+    }
+}
+fn main() {
+    wasm_logger::init(wasm_logger::Config::default());
+    yew::Renderer::<App>::new().render();
+}
---
+++ view/src/nav.rs
@@ -0,0 +1,40 @@
+use yew::prelude::*;
+
+#[function_component(NavigationBar)]
+fn Nav() -> Html {
+    html! {
+                    <>
+                        <head>
+
+                  <link href="https://fonts.googleapis.com/css2?family=Oswald&display=swap" rel="stylesheet"/>
+                 <link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet"/>
+                 <link rel="preconnect" href="https://fonts.googleapis.com"/>
+    <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet"/>
+                  </head>
+
+                  <body>
+                  <h2 class="TEST">{"測試"} </h2>
+                    <input type="checkbox" id="active"/>
+                    <label for="active" class="menu-btn"><span></span></label>
+                    <label for="active" class="close"></label>
+                    <div class="wrapper">
+                      <ul>
+                <li><a href="#">{"Home"}</a></li>
+                <li><a href="#">{"About"}</a></li>
+                <li><a href="#">{"Services"}</a></li>
+                <li><a href="#">{"Gallery"}</a></li>
+                <li><a href="#">{"Feedback"}</a></li>
+                </ul>
+                </div>
+                <div class="content">
+                      <div class="title">{"
+                Fullscreen Overlay Navigation Bar"}</div>
+                <p>
+                {"(Hamburger Menu-2)"} </p>
+                </div>
+                </body>
+            </>
+                    }
+}
+
+

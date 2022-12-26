fb60b15c32eb2f5af5cae88400f550cb0fde4f19
rex <rex>
2022-12-26 21:45:59 +0800

    home page

--- pages/home/src/lib.rs
+++ pages/home/src/lib.rs
@@ -2,6 +2,7 @@ use log::info;
 use stylist::style;
 use view::{footer, nav};
 use yew::prelude::*;
+use utils::theme_provider;
 #[function_component(Home)]
 pub fn home() -> Html {
     let style = style!(
@@ -23,42 +24,79 @@ pub fn home() -> Html {
             max-width: 1024px;
         }
       .kanban {
+          box-shadow: 0 0.5rem 2rem rgb(0 0 0 / 12%);
         position:relative;
         overflow: hidden;
         border-radius: 0.5rem;
         display:flex;
         width:100%;
-        grid-gap: 15px;
         margin-left:20px;
       } 
       .kanban_meta_data {
         overflow: hidden;
         width:15%; 
-        justify-content: center;
         flex-direction: column;
+        justify-content: center;
         display: flex;
-        grid-gap: 2px;
+      }
+      .kanban_meta_data * {
+      padding-left: 15px
+      }
+      .kanban_content {
+      padding-left:0px;
       }
       time {
       color:green;
       }
+      .kanban:hover .big_pic {
+        opacity: 0.5;
+      }
       .grid-2_container{
-        justify-content: center;
-        flex-direction: column;
-          display: grid;
+      margin-left: 20px;
+    display: flex;
+    margin-left: 25px;
+    margin: auto;
+    max-width: 1024px;
+    padding-left: 20px;
       }
 
       .grid-2_article{
-          flex: 1;
+       box-shadow: 0 0.5rem 2rem rgb(0 0 0 / 12%);
+       width: 50%;
+       border-radius: 0.5rem;
+       height: 50%;
+       display: grid;
+       margin-left:25px;
       }       
       .regular_pic {
         position:relative;
         background-size: contain;
         background-repeat: no-repeat;
         display: flex;
-        width: 35%;
-        height: 35%;
+        width: 100%;
       }
+      .grid-2_article_metadata {
+      height: auto;
+      }
+      .grid-2_article_metadata * {
+      padding-left: 15px;
+      }
+     .giu_content {
+        height:0;
+        opacity: 0;
+        padding-left:0px;
+    }
+      .grid-2_article:hover .giu_content {
+      height:75px;
+      overflow: hidden;
+      opacity: 1;
+  transition: opacity 1.1s ease-out;
+  
+     }
+      .grid-2_article:hover .regular_pic {
+  transition: opacity 0.1s ease-out;
+  opacity: 0.5;
+}
     "
     )
     .unwrap();
@@ -86,11 +124,25 @@ pub fn home() -> Html {
                     <article class="grid-2_article">
                       <img class="regular_pic" src="https://live.staticflickr.com/65535/52573392542_eeb51ca196_4k.jpg"/>
                       <div class="grid-2_article_metadata">
+                      <time>{"March 05, 2019"}</time>
+                      <h2>{"Emoji Support"}</h2>
+                      <div>
+                          <p class="giu_content">
+                          {"Emoji can be enabled in a Hugo project in a number of ways."}
+                          </p>
+                      </div>
                       </div>
                     </article>
                     <article class="grid-2_article">
                       <img class="regular_pic" src="https://live.staticflickr.com/65535/52573392542_eeb51ca196_4k.jpg"/>
                       <div class="grid-2_article_metadata">
+                      <time>{"March 05, 2019"}</time>
+                      <h2>{"Emoji Support"}</h2>
+                      <div>
+                          <p class="giu_content">
+                          {"Emoji can be enabled in a Hugo project in a number of ways."}
+                          </p>
+                      </div>
                       </div>
                     </article>
                   </div>
